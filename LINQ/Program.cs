cabda935574813d663dd2a48f0166d9649237917
Selleck Elliott <selleck.elliott@gmail.com>
2024-06-19 12:54:59 -0600

    length ordered LINQ list

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace LINQ
+﻿using System.Linq;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> breakfastFood = new List<string>();
+
+            breakfastFood.Add("Waffles");
+            breakfastFood.Add("German Pancake");
+            breakfastFood.Add("Biscuits and Gravy");
+            breakfastFood.Add("Aloo Dosa");
+            breakfastFood.Add("Farina");
+            breakfastFood.Add("Poori");
+            breakfastFood.Add("Steak and Eggs");
+
+            var plate = breakfastFood.OrderBy(x => x.Length).ToList();
+
+            foreach (var food in plate)
+            {
+                Console.WriteLine(food);
+            }
         }
     }
 }
